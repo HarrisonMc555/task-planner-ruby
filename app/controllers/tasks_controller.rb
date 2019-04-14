aa6ca6eacf8e49338c675aa0bf1ce4e2c4d4e57b
Harrison McCullough <mccullough.harrison@gmail.com>
2019-04-14 08:02:01 -0600

    Add CRU operations (with buggy update)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,13 +8,27 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
     @task = Task.new(task_params)
 
-    @task.save
-    redirect_to @task
+    if @task.save
+      redirect_to @task
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
   end
 
   private
