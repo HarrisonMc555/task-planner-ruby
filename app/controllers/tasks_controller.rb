8870792e002b841bcef8c9f31549b96486d43267
Harrison McCullough <mccullough.harrison@gmail.com>
2019-04-14 12:20:27 -0600

    Fix bug with task edit controller/view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,6 +11,10 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def create
     @task = Task.new(task_params)
 
