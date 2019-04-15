c7badac580b7e4257da708dfa35c6b0d0734c9d7
Harrison McCullough <mccullough.harrison@gmail.com>
2019-04-15 10:34:15 -0600

    Add delete/destroy for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,6 +35,13 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :description)
